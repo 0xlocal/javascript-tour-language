db24b7178be97b4bd58c7ab0bde90b53532dc611
0xlocal <ferreluchiha@gmail.com>
2021-11-03 17:20:31 +0700

    add data structure

---
+++ basics/data-structure.js
@@ -0,0 +1,22 @@
+/**
+ * * DATA TYPES
+ * * Theres seven data types that are primitives:
+ * * Boolean, true and false.
+ * * null, for null value. (because javascript is case-sensitive, null is different with Null, NULL, and other variant).
+ * * undefined. A top-level property whose value is not defined.
+ * * Number. An integer or floating point number. Example: 42 or 3.14159
+ * * BigInt. An Integer with arbitary precision. Example: 9007199254740992n.
+ * * String. A sequence of characters that represent a text value. Example: "Hello World".
+ * * Symbol. A data type whose instances are unique and immutable (new in ECMAScript 2015).
+ * * Object. You can think of objects as named containers for values, and functions as procedures that you script can perform.
+ */
+
+const isBoolean = true;
+let moneyPocket = null;
+let girlFriend = undefined;
+let myAge = 22;
+let myBigInt = 3429348n;
+let myName = "Ferrel John Fernando";
+let mySymbol = Symbol("Sym");
+let myObj = { my_key: "my_value" };
+let myArray = [1, 2, 3];
